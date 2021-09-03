8e8494036c2514c33de3d484dd29bbfeae5b354c
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-09-03 20:59:22 +0300

    Create hello_name.cs

---
+++ hello_name.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+
+{
+    class Programming
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input u name: ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+
+            Console.WriteLine("\n");
+
+            Console.Read();
+
+
+        }
+    }
+}
