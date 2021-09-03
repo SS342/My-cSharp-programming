3a252b7f4d096771755b48835c5e62c965784ad8
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-09-03 21:11:46 +0300

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+
+{
+    class Programming
+    {
+        static void Main(string[] args)
+        {
+            int q = 99;
+            Console.WriteLine(q);
+
+            string s = "It`s string!";
+
+            Console.WriteLine(s);
+
+            int i = 8;
+            string s1 = "Hello c#";
+            double d = 1.0;
+
+            Console.WriteLine(i + s1 + d);
+
+            var i1 = 1221;
+
+            var s2 = " hello c#";
+
+            var d1 = 1.0;
+
+            Console.WriteLine(i1 + s2 + d1);
+
+        }
+    }
+}
