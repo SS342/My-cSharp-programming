9d3b5196e33ff6a35edb60040a061a4de22a5529
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-09-04 22:14:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace A
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pa-Ra-Pa-Pa");
+
+            string sentense = "";
+
+            for (; ; )
+            {
+                Console.WriteLine("Input");
+                string line = Console.ReadLine();
+
+                string[] terms = { "EXIT", "QUIT", "exit", "quit" };
+
+                bool quitting = false;
+
+                foreach (string term in terms)
+                {
+                    if (String.Compare(line, term) == 0)
+                    {
+                        quitting = true;
+                    }
+                }
+                if (quitting == true)
+                {
+                    break;
+                }
+                sentense = String.Concat(sentense, line);
+                Console.WriteLine("\n You Input:\n" + sentense);
+            }
+        }
+    }
+}
